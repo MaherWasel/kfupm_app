b9142b90afc0312ed0783503bc338247b9c0d699
Abdullah Fawzi <abdullahfawzi1684@gmail.com>
2023-11-30 21:26:40 +0300

    flutterfire_cli installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kfupm_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
