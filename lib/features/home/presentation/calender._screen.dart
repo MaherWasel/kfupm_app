fe74fe27e84d9276e74c73524e974c9b81984949
Abdullah Fawzi <abdullahfawzi1684@gmail.com>
2023-12-02 00:30:57 +0300

    styling homescreen background done

--- lib/features/home/presentation/calender._screen.dart
+++ lib/features/home/presentation/calender._screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:kfupm_app/features/home/presentation/home_page_appbar.dart';
 
 class CalenderScreen extends StatelessWidget {
   const CalenderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final deviceData = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: HomePageAppBar(),
+      appBar: AppBar(),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        height: deviceData.height,
+        width: deviceData.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(147, 128, 50, 1),
+              Color.fromRGBO(14, 95, 50, 1),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Image.asset(
+          "assets/planer.png",
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -3,21 +3,31 @@ import 'package:kfupm_app/features/home/presentation/anouncments/anouncments.dar
 import 'package:kfupm_app/features/home/presentation/home_page_appBar.dart';
 import 'package:kfupm_app/features/home/presentation/home_page_bottomBar.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: HomePageAppBar(),
+    final deviceData = MediaQuery.of(context).size;
+    return Scaffold(
       bottomNavigationBar: HomePageBottomBar(),
-      body: Column(
-        children: [
-          Announcments()
-        ],
+      body: Container(
+        height: deviceData.height,
+        width: deviceData.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(147, 128, 50, 1),
+              Color.fromRGBO(14, 95, 50, 1),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [HomePageAppBar(), Announcments()],
+        ),
       ),
-      
     );
   }
-
 }
