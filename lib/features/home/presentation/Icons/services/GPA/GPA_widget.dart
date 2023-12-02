d9fcd7f8ec50d0cd5dcdcdf257bfb64c3acf3fb1
Yousuf <yousefsa2011@gmail.com>
2023-12-02 17:25:19 +0300

    comment gpa

--- lib/features/home/presentation/Icons/services/GPA/GPA_widget.dart
+++ lib/features/home/presentation/Icons/services/GPA/GPA_widget.dart
@@ -11,10 +11,8 @@ class GpaWidget extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(Sizes.p16),
       decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 255, 255, 255),
-
-        borderRadius: BorderRadius.circular(Sizes.p8)
-      ),
+          color: const Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(Sizes.p8)),
       padding: const EdgeInsets.all(10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -24,41 +22,38 @@ class GpaWidget extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
             ),
             child: Container(
-              width: deviceSizes.width/2,
+              width: deviceSizes.width / 2,
               margin: EdgeInsets.all(Sizes.p8),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(Sizes.p12),
-                color: Color.fromARGB(255, 8, 108, 11)
-                
-              ),
+                  borderRadius: BorderRadius.circular(Sizes.p12),
+                  color: Color.fromARGB(255, 8, 108, 11)),
               child: Column(children: [
-                SizedBox(height: Sizes.p12,),
-                Text("Gpa & Badge",
-                style: midTextL.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-                ),),
-                Text(gpa.toString(),
-                style: midTextL.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-                )),
-                SizedBox(height: Sizes.p12,
+                SizedBox(
+                  height: Sizes.p12,
                 ),
-
+                Text(
+                  "Gpa & Badge",
+                  style: midTextL.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text(gpa.toString(),
+                    style: midTextL.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: Sizes.p12,
+                ),
               ]),
             ),
           ),
           Container(
             decoration: BoxDecoration(
-             color: Color.fromARGB(255, 8, 108, 11),
-             borderRadius: BorderRadius.circular(Sizes.p16)
-            
+                color: Color.fromARGB(255, 8, 108, 11),
+                borderRadius: BorderRadius.circular(Sizes.p16)),
+            child: Icon(
+              Icons.star,
+              size: 100,
+              color: Color.fromARGB(255, 215, 194, 6),
             ),
-            child: Icon(Icons.star,
-            
-            size: 100,
-            color: Color.fromARGB(255, 215, 194, 6),),
           )
         ],
       ),
